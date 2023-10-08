d2332b6d2b5db3a0a388b7f3fc194c52240f8dfe
Joel <joelmantere@gmail.com>
2023-10-08 18:12:13 +0300

    Beginning of task 2.11

--- PhoneBook/src/main.jsx
+++ PhoneBook/src/main.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import App from './App.jsx'
+
+const promise = axios.get("http://localhost:3000/persons").then((result) => {
+    result.data.forEach(element => {
+        console.log(element);
+    });
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
